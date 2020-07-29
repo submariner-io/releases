a4be85ab375ccea1bcf76ccec08cd0d60f7bd041
Mike Kolesnik <mkolesni@redhat.com>
2020-07-29 16:00:32 +0300

    Add commit validations
    The validation script will now validate that the commits are actual tags
    or hashes that exist on the repository.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,24 +4,24 @@ set -e
 
 source ${DAPPER_SOURCE}/scripts/lib/yaml_funcs
 
+readonly PROJECTS=(admiral lighthouse shipyard submariner submariner-charts submariner-operator)
+
 function validate_file_fields() {
     local missing=0
 
     function _validate() {
         local key=$1
         validate_value $file $key || missing=$((missing+1))
+        release[$key]=$(get_value $file $key)
     }
 
     _validate 'version'
     _validate 'name'
     _validate 'release-notes'
     _validate 'components'
-    _validate 'components.admiral'
-    _validate 'components.lighthouse'
-    _validate 'components.shipyard'
-    _validate 'components.submariner'
-    _validate 'components.submariner-charts'
-    _validate 'components.submariner-operator'
+    for project in ${PROJECTS[*]}; do
+        _validate "components.${project}"
+    done
 
     if [[ $missing -gt 0 ]]; then
         printerr "Missing ${missing} fields"
@@ -30,13 +30,30 @@ function validate_file_fields() {
 }
 
 function validate_file() {
+    declare -A release
     validate_file_fields
 
-    version=$(get_value $file 'version')
+    version=${release['version']}
     if ! git check-ref-format "refs/tags/${version}"; then
         printerr "Version ${version@Q} is not a valid tag name"
         return 1
     fi
+
+    rm -rf projects
+    mkdir -p projects
+    pushd projects
+    for project in ${PROJECTS[*]}; do
+        mkdir -p "${project}"
+        git init "${project}"
+        pushd "${project}"
+        git remote add origin "https://github.com/submariner-io/${project}"
+        git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin "+${release["components.${project}"]}:refs/remotes/origin/master"
+        git checkout --progress --force -B master refs/remotes/origin/master
+        popd
+    done
+
+    popd
+    rm -rf projects
 }
 
 for file in $(find releases -type f); do
