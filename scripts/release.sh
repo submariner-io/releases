29fa785d064529dc13f087e9fdd6ab4220125d07
Mike Kolesnik <mkolesni@redhat.com>
2020-10-06 16:06:11 +0300

    Release a "release" on each project once target is merged
    This will make sure that the projects are properly tagged.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,4 +7,21 @@ source ${DAPPER_SOURCE}/scripts/lib/utils
 file=$(readlink -f releases/target)
 read_release_file
 
+gh config set prompt disabled
 gh release create "${release['version']}" --title "${release['name']}" --notes "${release['release-notes']}"
+
+export GITHUB_TOKEN="${RELEASE_TOKEN}"
+
+origin_url=$(git config --get remote.origin.url)
+if [[ "$origin_url" =~ ^https:.* ]]; then
+    release_repo=$(echo "$origin_url" | cut -f 4 -d'/')
+elif [[ "$origin_url" =~ ^git@.* ]]; then
+    release_repo=$(echo "$origin_url" | cut -f 4- -d'/' | cut -f 1 -d '.')
+else
+    echo "ERROR: Can't parse origin URL to extract origin repo: ${origin_url}"
+    exit 1
+fi
+
+for project in ${PROJECTS[*]}; do
+    gh release create "${release['version']}" --title "${release['name']}" --notes "${release['release-notes']}" --repo "${release_repo}/${project}" --target "${release["components.${project}"]}"
+done
