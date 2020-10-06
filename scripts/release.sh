ffbcafbf9fca646c7290949ce016b483cf12cd6d
Mike Kolesnik <mkolesni@redhat.com>
2020-10-06 16:06:11 +0300

    Release a "release" on each project once target is merged
    This will make sure that the projects are properly tagged.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,4 +7,30 @@ source ${DAPPER_SOURCE}/scripts/lib/utils
 file=$(readlink -f releases/target)
 read_release_file
 
+gh config set prompt disabled
 gh release create "${release['version']}" --title "${release['name']}" --notes "${release['release-notes']}"
+
+export GITHUB_TOKEN="${RELEASE_TOKEN}"
+
+origin_url=$(git config --get remote.origin.url)
+if [[ "$origin_url" =~ ^https:.* ]]; then
+    release_repo=$(echo "$origin_url" | cut -f 4 -d'/')
+elif [[ "$origin_url" =~ ^git@.* ]]; then
+    release_repo=$(echo "$origin_url" | cut -f 4- -d'/' | cut -f 1 -d '.')
+else
+    printerr "Can't parse origin URL to extract origin repo: ${origin_url}"
+    exit 1
+fi
+
+errors=0
+for project in ${PROJECTS[*]}; do
+    clone_repo
+    commit_ref=$(_git rev-parse --verify HEAD)
+    gh release create "${release['version']}" --title "${release['name']}" --notes "${release['release-notes']}" --repo "${release_repo}/${project}" --target "$commit_ref" || errors=$((errors+1))
+done
+
+if [[ $errors > 0 ]]; then
+    printerr "Failed to create release on ${errors} projects."
+    exit 1
+fi
+
