ba15139aa93f5e4fb51c35a2d92e017eee837971
Mike Kolesnik <mkolesni@redhat.com>
2020-10-20 11:49:41 +0300

    Build subctl instead of downloading
    This should enable us to release the subctl binary on the releases in
    the releases repository instead of the operator one.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/subctl.sh
+++ scripts/subctl.sh
@@ -2,14 +2,20 @@
 
 set -e
 
-source ${SCRIPTS_DIR}/lib/utils
 source ${DAPPER_SOURCE}/scripts/lib/utils
+source ${SCRIPTS_DIR}/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/utils
 
 file=$(readlink -f releases/target)
 read_release_file
 
-export VERSION="${release["components.submariner-operator"]}"
-curl -Ls https://get.submariner.io | bash
+project=submariner-operator
+clone_repo
 
-ln -f -s /root/.local/bin/subctl /go/bin/subctl
-subctl version
+pushd projects/submariner-operator
+export VERSION="${release["version"]}"
+make bin/subctl
+ln -f -s $(pwd)/bin/subctl /go/bin/subctl
+./bin/subctl version
+popd
+
