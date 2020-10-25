6ce6203ebfe1d19e6787a45bea2720467f925920
Mike Kolesnik <mkolesni@redhat.com>
2020-10-25 16:33:01 +0200

    Upload tagged images to Quay
    The release job will upload images tagged with the current release tag
    to quay.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+source ${DAPPER_SOURCE}/scripts/lib/image_defs
 source ${DAPPER_SOURCE}/scripts/lib/utils
 
 file=$(readlink -f releases/target)
@@ -9,6 +10,9 @@ read_release_file
 
 gh config set prompt disabled
 gh release create "${release['version']}" projects/submariner-operator/dist/subctl-* --title "${release['name']}" --notes "${release['release-notes']}"
+
+# Creating a local tag so that images are uploaded with it
+git tag -f "${release['version']}"
 
 export GITHUB_TOKEN="${RELEASE_TOKEN}"
 
@@ -33,4 +37,13 @@ if [[ $errors > 0 ]]; then
     printerr "Failed to create release on ${errors} projects."
     exit 1
 fi
+
+images=""
+for project in ${PROJECTS[*]}; do
+    for image in ${project_images[${project}]}; do
+        images+=" $image"
+    done
+done
+
+make release RELEASE_ARGS="$images --tag ${release['version']}"
 
