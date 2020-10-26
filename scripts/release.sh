20055c71c98f2d4ccea1d605bb5f5243a57800e9
Mike Kolesnik <mkolesni@redhat.com>
2020-10-26 16:25:27 +0200

    Refactor release script
    This should make it easier to follow and add functionality to
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,34 +3,47 @@
 set -e
 
 source ${DAPPER_SOURCE}/scripts/lib/utils
+source ${SCRIPTS_DIR}/lib/debug_functions
+
+### Functions ###
+
+function determine_org() {
+    git config --get remote.origin.url | awk -F'[:/]' '{print $(NF-1)}'
+}
+
+function create_release() {
+    local project="$1"
+    local target="$2"
+    local files="${@:3}"
+    local org=$(determine_org)
+
+    gh config set prompt disabled
+    gh release create "${release['version']}" $files \
+        --title "${release['name']}" \
+        --repo "${org}/${project}" \
+        --target "${target}" \
+        --notes "${release['release-notes']}"
+}
+
+### Main ###
 
 file=$(readlink -f releases/target)
 read_release_file
+errors=0
 
-gh config set prompt disabled
-gh release create "${release['version']}" projects/submariner-operator/dist/subctl-* --title "${release['name']}" --notes "${release['release-notes']}"
+commit_ref=$(git rev-parse --verify HEAD)
+create_release releases "${commit_ref}" projects/submariner-operator/dist/subctl-* || errors=$((errors+1))
 
 export GITHUB_TOKEN="${RELEASE_TOKEN}"
 
-origin_url=$(git config --get remote.origin.url)
-if [[ "$origin_url" =~ ^https:.* ]]; then
-    release_repo=$(echo "$origin_url" | cut -f 4 -d'/')
-elif [[ "$origin_url" =~ ^git@.* ]]; then
-    release_repo=$(echo "$origin_url" | cut -f 4- -d'/' | cut -f 1 -d '.')
-else
-    printerr "Can't parse origin URL to extract origin repo: ${origin_url}"
-    exit 1
-fi
-
-errors=0
 for project in ${PROJECTS[*]}; do
     clone_repo
     commit_ref=$(_git rev-parse --verify HEAD)
-    gh release create "${release['version']}" --title "${release['name']}" --notes "${release['release-notes']}" --repo "${release_repo}/${project}" --target "$commit_ref" || errors=$((errors+1))
+    create_release "${project}" "${commit_ref}" || errors=$((errors+1))
 done
 
 if [[ $errors > 0 ]]; then
-    printerr "Failed to create release on ${errors} projects."
+    printerr "Encountered ${errors} errors while doing the release."
     exit 1
 fi
 
