ca8cc8153ca9fadad0b2078a84b2af5aa9d58b84
Mike Kolesnik <mkolesni@redhat.com>
2021-07-13 09:56:15 +0300

    Fix subctl building
    Since now `Dockerfile.dapper` lives in Shipyard, we need to make sure it
    exists before trying to modify it
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/subctl.sh
+++ scripts/subctl.sh
@@ -11,15 +11,18 @@ set -e
 
 # Allow running dapper in dapper by some trickery
 function dapper_in_dapper() {
+    # Trick our own Makefile to think we're running outside dapper
+    export DAPPER_HOST_ARCH=""
+
+    # Make sure we have Dockerfile.dapper as it could be in Shipyard and needs to be fetched
+    make Dockerfile.dapper
+
     # Plant a directive in the Dockerfile.dapper to let dapper run in dapper
     local orig_pwd
     local cur_pwd
     orig_pwd=$(docker inspect "$HOSTNAME" | jq -r ".[0].Mounts[] | select(.Destination == \"$DAPPER_SOURCE\") | .Source")
     cur_pwd=$(pwd | sed -E 's/[a-zA-Z0-9-]+/../g')
     echo "ENV DAPPER_CP=${cur_pwd}/${orig_pwd}/projects/submariner-operator" >> Dockerfile.dapper
-
-    # Trick our own Makefile to think we're running outside dapper
-    export DAPPER_HOST_ARCH=""
 }
 
 ### Main ###
