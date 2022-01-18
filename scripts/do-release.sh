ac648b6e78137786a515a1e2ee033167c2f7ef72
Daniel Farrell <dfarrell@redhat.com>
2022-01-18 11:44:18 -0500

    Add e2e-all-k8s label to PRs
    Add a label to PRs created by the release process that will run E2E
    against all relevant K8s versions if that job is available in a repo.
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -88,7 +88,7 @@ function create_pr() {
     _git commit -a -s -m "${msg}"
     push_to_repo "${branch}"
     to_review=$(dryrun gh pr create --repo "${ORG}/${project}" --head "${branch}" --base "${base_branch}" --title "${msg}" \
-                --label "ready-to-test" --body "${msg}")
+                --label "ready-to-test" --label "e2e-all-k8s" --body "${msg}")
     dryrun gh pr merge --auto --repo "${ORG}/${project}" --squash "${to_review}" || echo "WARN: Failed to enable auto merge on ${to_review}"
     reviews+=("${to_review}")
 }
