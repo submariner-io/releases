b795400fee885cef33c17591a04728bf9448cc68
Mike Kolesnik <mkolesni@redhat.com>
2022-08-30 16:42:10 +0300

    Add `automated` label to PRs
    The PRs created by the release process (either by the `release` target
    or by the bot) should be labeled as `automated` as it makes them easier
    to filter.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -97,7 +97,7 @@ function create_pr() {
     _git commit -a -s -m "${msg}"
     push_to_repo "${branch}"
     to_review=$(dryrun gh pr create --repo "${ORG}/${project}" --head "${branch}" --base "${base_branch}" --title "${msg}" \
-                --label "ready-to-test" --label "e2e-all-k8s" --body "${msg}" 2>&1)
+                --label "automated" --label "ready-to-test" --label "e2e-all-k8s" --body "${msg}" 2>&1)
 
     # shellcheck disable=SC2181 # The command is too long already, this is more readable
     if [[ $? -ne 0 ]]; then
