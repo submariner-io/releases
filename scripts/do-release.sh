5ab486b97c0c96541c7fe3aafe0afd2df7aafce2
Stephen Kitt <skitt@redhat.com>
2022-09-06 09:48:30 +0200

    When upgrading releases, tidy before vendor
    go mod tidy now needs to run before go mod vendor, to ensure that all
    the required hashes in go.sum are populated.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -61,8 +61,8 @@ function clone_and_create_branch() {
 function _update_go_mod() {
     go mod tidy -compat=1.17
     GOPROXY=direct go get "github.com/submariner-io/${target}@${release['version']}"
-    go mod vendor
     go mod tidy -compat=1.17
+    go mod vendor
 }
 
 function update_go_mod() {
